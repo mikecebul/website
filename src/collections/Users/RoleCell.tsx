2e05d7650d8a9e05e0aa2b82a72e97a92c81749a
Mike Cebulski <mikecebul@gmail.com>
2026-03-25 22:21:37 -0400

    Refresh Payload admin theme and dashboard styling

--- src/collections/Users/RoleCell.tsx
+++ src/collections/Users/RoleCell.tsx
@@ -1,16 +1,17 @@
 import { DefaultCellComponentProps } from "payload"
 
 const RoleCell = ({ cellData }: DefaultCellComponentProps) => {
-
   switch (cellData) {
     case 'superAdmin':
       return 'Super Admin'
     case 'admin':
       return 'Admin'
-    case 'admin':
-      return 'Admin'
     case 'editor':
       return 'Editor'
+    case 'user':
+      return 'User'
+    default:
+      return cellData
   }
 }
 
